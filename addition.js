db1eec427d7791a151f127d35e731549db8deff6
nikhil <nikhil@nikhils-MacBook-Pro.local>
2020-05-21 23:51:02 +0530

    Serving static assests: html file; JSON object on get

---
+++ addition.js
@@ -0,0 +1,5 @@
+var exports = module.exports = {};
+
+exports.AddNumber=funtion(a,b) {
+    return a + b;
+}
--- server.js
+++ server.js
@@ -1,8 +1,14 @@
 const express = require('express');
 const app = express();
 
+app.use(express.static('public'))
+
 app.get('/', (request, response) => {
 	response.send("hello world");
+});
+
+app.get('/json', (request, response) => {
+    response.status(200).json({"name": "nikhil"});
 });
 
 app.listen(3000, () => {
