16c709cd07cecff0aaf986a80a7b4a08064b932d
nikhil <nikhil@nikhils-MacBook-Pro.local>
2020-05-26 22:59:44 +0530

    Adding middleware function with logger function to print call url & time

--- server.js
+++ server.js
@@ -1,7 +1,22 @@
 const express = require('express');
 const app = express();
 
+
+// Middleware
+// below logger are simple example of middleware
+// Can use for complex task like authorization & authentication
+const urlLogger = (request, response, next) => {
+  console.log('Request URL:', request.url);
+  next();
+};
+const timeLogger = (request, response, next) => {
+    console.log('Datetime:', new Date(Date.now()).toString());
+    next();
+}
+
 app.use(express.static('public'))
+// Every route uses these middleware functions with app.use, OR you can specify for every call
+app.use(urlLogger, timeLogger)
 
 app.get('/', (request, response) => {
 	response.send("hello world");
